e317ab056c3891bcbe65e421ba1e4a31a32c305f
Edouard Deschamps <edouarddeschamps@gmail.com>
2024-02-20 11:18:51 -0500

    premier commit TP1

---
+++ TP1/app/src/main/java/com/example/tp1/ListeProduits.java
@@ -0,0 +1,33 @@
+package com.example.tp1;
+
+import java.util.Hashtable;
+
+public class ListeProduits {
+
+    private Hashtable<String, Produit> liste;
+
+    public ListeProduits ()
+    {
+        liste = new Hashtable();
+        liste.put("Café filtre Petit",new Produit("Café","Petit",1.80 ,5 ) );
+        liste.put("Café filtre Moyen",new Produit("Café","Moyen",3 ,7 ));
+        liste.put("Café filtre Grand", new Produit("Café","Grand",3.96 ,10 ));
+        liste.put("Americano Petit",new Produit("Americano","Petit",2.40 ,9 ));
+        liste.put("Americano Moyen",new Produit("Americano","Moyen",4.00 ,11 ) );
+        liste.put("Americano Grand",new Produit("Americano","Grand",5.28 ,18 ) );
+        liste.put("Café glacé Petit",new Produit("Café glacé","Petit",2.50 ,10 ) );
+        liste.put("Café glacé Moyen",new Produit("Café glacé","Moyen",4.17 ,12 ) );
+        liste.put("Café glacé Grand",new Produit("Café glacé","Grand",5.50 ,20 ) );
+        liste.put("Latté Petit",new Produit("Latté","Petit",4.00 ,125 ) );
+        liste.put("Latté Moyen",new Produit("Latté","Moyen",6.67 ,208 ) );
+        liste.put("Latté Grand",new Produit("Latté","Grand",10.00 ,250 ) );
+    }
+
+    public Produit recupererProduit ( String cle)
+    {
+        Produit pvide = new Produit("","", 0,0);
+       return pvide ;
+    }
+
+
+}
---
+++ TP1/app/src/main/java/com/example/tp1/Produit.java
@@ -0,0 +1,12 @@
+package com.example.tp1;
+
+public class Produit {
+    private String noms;
+    private String format;
+    private double prix;
+    private int calories;
+
+    Produit(String n, String f, double p, int c){
+
+    }
+}
