47be0c5d1d82fc6671792cc5eb922e78f6fa5085
Edouard Deschamps <edouarddeschamps@gmail.com>
2024-02-21 11:31:11 -0500

    upgrade Ui et setup  des variables et fonctionnalité de bases

--- TP1/app/src/main/java/com/example/tp1/MainActivity.java
+++ TP1/app/src/main/java/com/example/tp1/MainActivity.java
@@ -3,8 +3,27 @@ package com.example.tp1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import java.util.Vector;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Vector<Produit> commande =  new Vector<>();
+
+    LinearLayout cafe_filtre, americano, cafe_glace, latte;
+
+    RadioGroup choixTaille;
+
+    TextView indicePrixCalorie, totalEtTaxes;
+
+    Button btnAjouter, btnEffacer;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
