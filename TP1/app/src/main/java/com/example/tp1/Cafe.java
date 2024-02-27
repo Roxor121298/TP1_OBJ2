2f57a1d7a2a69903bc722394db6fb1c37bf31723
Edouard Deschamps <edouarddeschamps@gmail.com>
2024-02-27 17:21:10 -0500

    superClass Café et fix le UI (encore)

--- TP1/app/src/main/java/com/example/tp1/Cafe.java
+++ TP1/app/src/main/java/com/example/tp1/Cafe.java
@@ -2,6 +2,10 @@ package com.example.tp1;
 
 public class Cafe extends Produit {
 
+    String nomscafe = "Cafe";
+    double prixCafe = 1.80;
+    int calorieCafe = 5;
+
     Cafe(String n, String f, double p, int c) {
         super(n, f, p, c);
     }
